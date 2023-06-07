0c4acb5b838e2299e1dd8bdc3184e93fb167dea2
bhagwan.5602@gmail.com <bhagwan@joinnextmed.com>
2023-06-07 13:02:49 +0530

    BACKEND - response types and enum file added

---
+++ server/modules/response.py
@@ -0,0 +1,31 @@
+
+# default global context
+context = {
+    "code": 0,
+    "msgCode": 20001,
+    "msgType": "Success",
+    "msgLang": "en",
+    "shortMsg": "",
+    "longMsg": "",
+    "internalMsg": "",
+    "enableAlert": False,
+    "displayMsgBy": "shortMsg",
+
+    "app_name": 'eLearning',
+    "app_domain": 'www.ecorpin.web.app',
+}
+
+# Global response message type
+response_message_type = {
+    "SUCCESS": "Success",
+    "FAILED": "Failed",
+    "ERROR": "Error",
+}
+
+response_message = {
+    "CREATED": "Successfully Created",
+    "UPDATED": "Successfully Updated",
+    "DELETED": "Successfully Deleted",
+    "GENERATED": "Successfully Generated",
+}
+
