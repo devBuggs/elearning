17d01e7d287eabb18a59c728cba50c59f6721d19
bhagwan.5602@gmail.com <bhagwan@joinnextmed.com>
2023-06-07 13:05:01 +0530

    BACKEND - webapp common views added

--- server/webapp/views.py
+++ server/webapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+# Custom Modules
+from modules.response import context
+
 # Create your views here.
+def index_view(request):
+    return render(request, 'webapp/index.html', context)
+
+def about_view(request):
+    return render(request, 'webapp/about.html', context)
+
+def contact_view(request):
+    return render(request, 'webapp/contact.html', context)
+
+def help_view(request):
+    return render(request, 'webapp/index.html', context)
+
+def faqs_view(request):
+    return render(request, 'webapp/index.html', context)
+
+def privacy_policy_view(request):
+    return render(request, 'webapp/index.html', context)
+
+def terms_conditions_view(request):
+    return render(request, 'webapp/index.html', context)
