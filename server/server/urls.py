0098a82794dedc023d834b8841327503d31373ad
Bhagwan Singh <bhagwan.5602@gmail.com>
2023-06-08 12:22:24 +0530

    BACKEND - global errors url updated to server

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,11 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler404 = 'webapp.views.custom_page_not_found_view'
+handler500 = 'webapp.views.custom_error_view'
+handler403 = 'webapp.views.custom_permission_denied_view'
+handler400 = 'webapp.views.custom_bad_request_view'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
