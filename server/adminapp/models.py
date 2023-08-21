41665faf06aa8a96850d8ca3171ddf3e7510ac17
Bhagwan Singh <bhagwan.5602@gmail.com>
2023-08-21 11:01:38 +0530

    dummy models added without fields for elearning

--- server/adminapp/models.py
+++ server/adminapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    pass
+
+class Profile(models.Model):
+    pass
+
+class SubscriptionPack(models.Model):
+    pass
+
+class UserSubscription(models.Model):
+    pass
+
+class BlogPost(models.Model):
+    pass
+
+# class 
+
+
+
+
+class Course(models.Model):
+    pass
+
+class Semester(models.Model):
+    pass
+
+class Subject(models.Model):
+    pass
+
+class Team(models.Model):
+    pass
+
